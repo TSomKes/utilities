f18ed23722f393aa71c136f8150751dff085bff2
TSomKes <tsomkes@gmail.com>
2017-01-05 10:21:30 -0600

    Changed start-tmux.sh
    -   Changed layouts:
        -   Code window:  3 --> 4 panes
        -   Notes window:  3 --> 2 panes
    -   Moved focus to left-hand pane in all windows
    -   Resized left-hand pane 5 columns to the right
        -   Really should learn how sizes work someday...
    -   + comments
    -   + todo file

--- start-tmux.sh
+++ start-tmux.sh
@@ -3,6 +3,10 @@
 # Inspired by various Google searches, but also by the book "tmux: productive
 # Mouse-Free Devleopment".
 
+# Hard-code-tastic!
+# - Relies on my usual directory structure
+# - I also use my own git aliases here
+
 sessionName=${PWD##*/}
 
 tmux has-session -t "$sessionName"
@@ -10,26 +14,44 @@ tmux has-session -t "$sessionName"
 # Create session if it doesn't already exist
 if [ $? != 0 ]
 then
-	# First window:  "code", current directory, 3 panes, main on left
+	# First window:  "code", current directory, 4 panes, main on left
 	tmux new-session -s "$sessionName" -n code -d
 	tmux split-window -t "$sessionName"
 	tmux split-window -t "$sessionName"
+	tmux split-window -t "$sessionName"
 	tmux select-layout -t "$sessionName" main-vertical
-	tmux send-keys -t "$sessionName":0.1 'ls' C-m
-	tmux send-keys -t "$sessionName":0.2 'git statz' C-m
 
-	# Second window:  "notes", ~/code/notes/, 3 panes, main on left
+    # Make sure the L pane is wide enough
+    # HACK - 'cause I haven't figured out how to size the L pane correctly.
+    tmux resize-pane -t "$sessionName":0.0 -R 5         
+
+    # U-R pane starts with `ls`
+	tmux send-keys -t "$sessionName":0.1 'ls' C-m
+
+    # L-R pane starts with `git statz`
+	tmux send-keys -t "$sessionName":0.3 'git statz' C-m
+
+    # Move focus to L pane (being nice to user)
+	tmux select-pane -t "$sessionName":0.0
+
+	# Second window:  "notes", ~/code/notes/, 2 panes, main on left
 	cd ~/code/notes
 	tmux new-window -t "$sessionName" -n notes
 	tmux split-window -t "$sessionName"
-	tmux split-window -t "$sessionName"
 	tmux select-layout -t "$sessionName" main-vertical
-	tmux send-keys -t "$sessionName":1.1 'ls' C-m
-	tmux send-keys -t "$sessionName":1.2 'git statz' C-m
 
-	# Select main pane of "code" window
-	tmux select-window -t "$sessionName":0.1
-	tmux select-pane -t "$sessionName":0.1
+    # Make sure the L pane is wide enough
+    # HACK - 'cause I haven't figured out how to size the L pane correctly.
+    tmux resize-pane -t "$sessionName":1.0 -R 5         
+
+    # R pane starts with `git statz`
+	tmux send-keys -t "$sessionName":1.1 'git statz' C-m
+
+    # Move focus to L pane (being nice to user)
+	tmux select-pane -t "$sessionName":1.0
+
+    # Set focus to L pane of first ("code") window (being nice to user)
+	tmux select-window -t "$sessionName":0.0
 fi
 
 tmux attach -t "$sessionName"
