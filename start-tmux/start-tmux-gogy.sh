50e0d5803c57ad8c142531d5f069c8a365fd2d86
TSomKes <tsomkes@gmail.com>
2017-08-22 10:20:54 -0500

    Made separate gogy DB tab
    Figured out that `mysql` accepts a `--prompt` argument.  Pretty happy
    about that.

--- start-tmux/start-tmux-gogy.sh
+++ start-tmux/start-tmux-gogy.sh
@@ -14,18 +14,17 @@ project="gogy"
 #ssh-add ~/.ssh/id_rsa
 ssh-add ~/.ssh/gogy_id_rsa
 
-# Third window:  "webservice", 5 panes, main on left
+# Second window:  "webservice", 4 panes, main on left
 cd ~/code/gogyup-web-services
 
 tmux new-window -t "$sessionName" -n webservice
 
 # Create one horizontal split, which leaves the right-hand pane active.  Then
-# split that pane three more times, leaving five total.
+# split that pane two more times, leaving four total.
 # Note: I tried playing with percentages, but that required using the -d flag
 # to keep new panes from getting focus, and then I had to futz with changing
 # percentages (25, 33, 50...)
 tmux split-window -t "$sessionName" -h
-tmux split-window -t "$sessionName"
 tmux split-window -t "$sessionName"
 tmux split-window -t "$sessionName"
 
@@ -41,19 +40,45 @@ tmux resize-pane -t "$sessionName":1.0 -R 5
 # U-R pane starts with ls
 tmux send-keys -t "$sessionName":1.1 'ls' C-m
 
-# UM-R pane starts with web service being served
+# M-R pane starts with web service being served
 tmux send-keys -t "$sessionName":1.2 'make serve-local' C-m
 
-# LM-R pane starts with DB (in theory)
-tmux send-keys -t "$sessionName":1.3 'cd db/' C-m
-tmux send-keys -t "$sessionName":1.3 'mysql --login-path=local' C-m
-
 # L-R pane starts with `git statz`
-tmux send-keys -t "$sessionName":1.4 'git fetch' C-m
-tmux send-keys -t "$sessionName":1.4 'git statz' C-m
+tmux send-keys -t "$sessionName":1.3 'git fetch' C-m
+tmux send-keys -t "$sessionName":1.3 'git statz' C-m
 
 # Move focus to L pane (being nice to user)
 tmux select-pane -t "$sessionName":1.0
+
+
+# Third window:  DB
+cd ~/code/gogyup-web-services/db
+
+tmux new-window -t "$sessionName" -n DB
+
+# Create one horizontal split, which leaves the right-hand pane active.  Then
+# split that pane again, leaving three total.
+tmux split-window -t "$sessionName" -h
+tmux split-window -t "$sessionName"
+
+tmux select-layout -t "$sessionName" main-vertical
+
+# Make sure the L pane is wide enough
+# HACK - Very specific to my current lappy, 'cause I haven't figured out
+# how to size the L pane correctly.
+tmux resize-pane -t "$sessionName":2.0 -R 5         
+
+# L pane starts connected to local DB 
+tmux send-keys -t "$sessionName":2.0 'mysql --login-path=local --database=gogy --prompt="local> "' Enter
+
+# U-R pane starts ready to connect to TEST DB
+tmux send-keys -t "$sessionName":2.1 'mysql --login-path=test --database=gogy --prompt="TEST> "'
+
+# L-R pane starts ready to connect to PROD DB
+tmux send-keys -t "$sessionName":2.2 'mysql --login-path=test --database=gogy --prompt="PROD> "'
+
+# Move focus to L pane (being nice to user)
+tmux select-pane -t "$sessionName":2.0
 
 
 # Fourth window:  "client", 4 panes, main on left
@@ -65,25 +90,24 @@ tmux split-window -t "$sessionName"
 tmux select-layout -t "$sessionName" main-vertical
 
 # Make sure the L panes are wide enough (see HACK)
-tmux resize-pane -t "$sessionName":2.0 -R 5         
+tmux resize-pane -t "$sessionName":3.0 -R 5         
 
 # L pane starts with `ls`
-tmux send-keys -t "$sessionName":2.0 'ls' C-m
+tmux send-keys -t "$sessionName":3.0 'ls' C-m
 
 # L-R pane starts with `git statz`
-tmux send-keys -t "$sessionName":2.3 'git fetch' C-m
-tmux send-keys -t "$sessionName":2.3 'git statz' C-m
+tmux send-keys -t "$sessionName":3.3 'git fetch' C-m
+tmux send-keys -t "$sessionName":3.3 'git statz' C-m
 
 # M-R pane starts with client site being served
-tmux send-keys -t "$sessionName":2.2 'cd litapp-source/www/' C-m
-tmux send-keys -t "$sessionName":2.2 'python3 -m http.server 8050' C-m
+tmux send-keys -t "$sessionName":3.2 'cd litapp-source/www/' C-m
+tmux send-keys -t "$sessionName":3.2 'python3 -m http.server 8050' C-m
 
 # Move focus to L pane (being nice to user)
-tmux select-pane -t "$sessionName":2.0
+tmux select-pane -t "$sessionName":3.0
 
 
 # Fire up some Chrome tabs
-google-chrome
 google-chrome 'https://gogyup-litapp.slack.com/messages/' &> /dev/null
 google-chrome 'https://www.toggl.com/app/timer' &> /dev/null
 google-chrome 'http://localhost:8000/datastore?type=user' &> /dev/null # Google App Engine
