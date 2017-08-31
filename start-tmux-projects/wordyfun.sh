715343de55e9a479fb7918f5856da394c2e17df5
TSomKes <tsomkes@gmail.com>
2017-08-31 11:45:49 -0500

    Add wordyfun start-tmux project

---
+++ start-tmux-projects/wordyfun.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Inspired by various Google searches, but also by the book "tmux: productive
+# Mouse-Free Devleopment".
+
+# Hard-code-tastic!
+# - Relies on my usual directory structure
+# - I also use my own git aliases here
+
+project="wordyfun"
+
+
+# Trigger an up-front blocking request for ssh passphrase
+ssh-add ~/.ssh/id_rsa
+
+
+# Third window:  "webservice", 5 panes, main on left
+cd ~/code/wordyfun
+
+tmux new-window -t "$sessionName" -n code
+
+# Create one horizontal split, which leaves the right-hand pane active.  Then
+# split that pane two more times, leaving four total.
+# Note: I tried playing with percentages, but that required using the -d flag
+# to keep new panes from getting focus, and then I had to futz with changing
+# percentages (25, 33, 50...)
+tmux split-window -t "$sessionName" -h
+tmux split-window -t "$sessionName"
+tmux split-window -t "$sessionName"
+
+tmux select-layout -t "$sessionName" main-vertical
+
+# Make sure the L pane is wide enough
+# HACK - Very specific to my current lappy, 'cause I haven't figured out
+# how to size the L pane correctly.
+tmux resize-pane -t "$sessionName":1.0 -R 5         
+
+# L pane starts with vim
+tmux send-keys -t "$sessionName":1.0 'vim notes.txt' Enter
+tmux send-keys -t "$sessionName":1.0 ':tabe game.py' Enter
+tmux send-keys -t "$sessionName":1.0 ':tabe data/' Enter
+tmux send-keys -t "$sessionName":1.0 '1gt' Enter
+
+# U-R pane starts with ls
+tmux send-keys -t "$sessionName":1.1 'ls' Enter
+
+# M-R pane starts with mongo
+tmux send-keys -t "$sessionName":1.2 'mongo' Enter
+
+# L-R pane starts with `git statz`
+tmux send-keys -t "$sessionName":1.3 'git fetch' Enter
+tmux send-keys -t "$sessionName":1.3 'git statz' Enter
+
+# Move focus to L pane (being nice to user)
+tmux select-pane -t "$sessionName":1.0
