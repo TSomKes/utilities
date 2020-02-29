2bd1e8661185be67403f24f0ce4c8ca18807355f
TSomKes <tsomkes@gmail.com>
2020-02-29 16:41:08 -0600

    Add background window

--- tmuxz-projects/gud.sh
+++ tmuxz-projects/gud.sh
@@ -1,19 +1,30 @@
 project="gud"
 projectDir=~/code/gogyup-dyslexia-client
 winCode="code"
+winBackground="background"
 
 tmux new-session -s "$project" -n "$winCode" -c "$projectDir" -d
 
-# 4 panes, main (left) just over 80 columns
+
+# Code window:  3 panes, main (left) just over 80 columns
 tmux split-window -t "$project":"$winCode" -c "$projectDir" -d
 tmux split-window -t "$project":"$winCode" -c "$projectDir" -d
-tmux split-window -t "$project":"$winCode" -c "$projectDir" -d
-tmux select-layout -t "$project":"$winCode" "f416,159x46,0,0{89x46,0,0,0,69x46,90,0[69x14,90,0,3,69x14,90,15,2,69x16,90,30,1]}"
+tmux select-layout -t "$project":"$winCode" "4d18,159x46,0,0{89x46,0,0,0,69x46,90,0[69x22,90,0,1,69x23,90,23,2]}"
 
 tmux send-keys -t "$project":"$winCode".0 "ls" Enter
+#tmux send-keys -t "$project":"$winCode".1 
+tmux send-keys -t "$project":"$winCode".2 "git statz" Enter
 
-tmux send-keys -t "$project":"$winCode".1 "make serve" Enter
 
-#tmux send-keys -t "$project":"$winCode".2 "postman" Enter
+# Background window:  4 panes, tiled
+tmux new-window -t "$project" -n "$winBackground" -c "$projectDir" -d
 
-tmux send-keys -t "$project":"$winCode".3 "git statz" Enter
+tmux split-window -t "$project":"$winBackground" -c "$projectDir" -d
+tmux split-window -t "$project":"$winBackground" -c "$projectDir" -d
+tmux split-window -t "$project":"$winBackground" -c "$projectDir" -d
+
+tmux select-layout -t "$project":"$winBackground" tiled
+
+tmux send-keys -t "$project":"$winBackground".0 "code" Enter
+tmux send-keys -t "$project":"$winBackground".1 "make serve" Enter
+tmux send-keys -t "$project":"$winBackground".2 "postman" Enter
