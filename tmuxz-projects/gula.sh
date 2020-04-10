d2a8b7bb63577bcb8ac444d4d9e3fbac62880d26
TSomKes <tsomkes@gmail.com>
2020-04-10 13:54:14 -0500

    Add GU litapp tmuxz project

---
+++ tmuxz-projects/gula.sh
@@ -0,0 +1,15 @@
+project="gula"
+projectDir=~/code/gogyup-literacy-app
+winCode="code"
+
+tmux new-session -s "$project" -n "$winCode" -c "$projectDir" -d
+
+
+# Code window:  3 panes, main (left) just over 80 columns
+tmux split-window -t "$project":"$winCode" -c "$projectDir" -d
+tmux split-window -t "$project":"$winCode" -c "$projectDir" -d
+tmux select-layout -t "$project":"$winCode" "4d18,159x46,0,0{89x46,0,0,0,69x46,90,0[69x22,90,0,1,69x23,90,23,2]}"
+
+tmux send-keys -t "$project":"$winCode".0 "code --folder-uri $projectDir" Enter
+tmux send-keys -t "$project":"$winCode".1 "ls" Enter
+tmux send-keys -t "$project":"$winCode".2 "git statz" Enter
