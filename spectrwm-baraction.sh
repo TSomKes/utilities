2846e9c2e53bbccefabb967ad86ddb654e15eda1
TSomKes <tsomkes@gmail.com>
2020-04-28 13:46:24 -0500

    Add screen brightness level display (0-15/15)
    - Rearrange script to match visual display order
    - Remove "empty" placeholder characters to make consistent white space
        - Means the bar "hops around" when these characters appear &
          disappear.  I don't mind.

--- spectrwm-baraction.sh
+++ spectrwm-baraction.sh
@@ -4,23 +4,15 @@
 SLEEP_SEC=5  
 while :; do
 
-    # Battery (e.g. -75%, +42%)
-    B_CAP=`cat /sys/class/power_supply/BAT0/capacity`
-
-    B_SYM=' '
-    B_STATE=`cat /sys/class/power_supply/BAT0/status`
-    if [ $B_STATE = 'Charging' ]; then
-        B_SYM='+'
-    elif [ $B_STATE = 'Discharging' ]; then
-        B_SYM='-'
-    fi
+    # Screen brightness
+    BRIGHT=`cat /sys/class/backlight/acpi_video0/actual_brightness`
 
     # Volume
     V_LVL=`amixer sget Master \
         | awk 'NR==5{print $4}' \
         | sed "s/[^0-9]//g"`
 
-    V_MUTE=' '
+    V_MUTE=''
     V_STATE=`amixer sget Master \
         | awk 'NR==5{print $6}' \
         | sed "s/[^a-z]//g"`
@@ -28,7 +20,18 @@ while :; do
         V_MUTE='m'
     fi
 
-    echo "${V_MUTE}${V_LVL}% | ${B_SYM}${B_CAP}%"
+    # Battery (e.g. -75%, +42%)
+    B_CAP=`cat /sys/class/power_supply/BAT0/capacity`
+
+    B_SYM=''
+    B_STATE=`cat /sys/class/power_supply/BAT0/status`
+    if [ $B_STATE = 'Charging' ]; then
+        B_SYM='+'
+    elif [ $B_STATE = 'Discharging' ]; then
+        B_SYM='-'
+    fi
+
+    echo "${BRIGHT} | ${V_MUTE}${V_LVL}% | ${B_SYM}${B_CAP}% | "
 
     sleep $SLEEP_SEC
 done
