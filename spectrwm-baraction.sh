ec065c9c63843bcd9b58abd1cea0213ade76b98f
TSomKes <tsomkes@gmail.com>
2024-02-27 19:07:43 -0600

    change spectrwm bar
    Not 100% great, but eh?

--- spectrwm-baraction.sh
+++ spectrwm-baraction.sh
@@ -28,9 +28,12 @@ while :; do
     B_STATE=`cat /sys/class/power_supply/BAT0/status`
     if [ $B_STATE = 'Disharging' ]; then
         B_SYM='-'
+    elif [ $B_STATE = 'Charging' ]; then
+        B_SYM='+'
     else
         # both 'Charging' and 'Unknown' seem to indicate charging
-        B_SYM='+'
+        # ...except recent experience has shown it happening while discharging
+        B_SYM='?'
     fi
 
     echo " ${RELATIVE_BRIGHT}% | v ${V_MUTE}${V_LVL} | b ${B_SYM}${B_CAP}% | "
