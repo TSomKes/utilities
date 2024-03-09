592e85a8cee0ee546338a3ee70fb1d3f75bfb317
TSomKes <tsomkes@gmail.com>
2024-03-09 15:09:00 -0600

    Add PA-script symlink script

---
+++ create-symlinks-pa.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -s $HOME/code/utilities/capture-aside.sh $HOME/.local/bin/capture-aside
+ln -s $HOME/code/utilities/tmux-popup-aside.sh $HOME/.local/bin/tmux-popup-aside
+
+ln -s $HOME/code/utilities/capture-doing.sh $HOME/.local/bin/capture-doing
+ln -s $HOME/code/utilities/tmux-popup-doing.sh $HOME/.local/bin/tmux-popup-doing
+
