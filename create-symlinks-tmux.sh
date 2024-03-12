da8fc47cdfec723351bf79f3768bcf98f5bf5a3b
TSomKes <tsomkes@gmail.com>
2024-03-12 13:39:49 -0500

    Make symlink helper script for tmux commands

---
+++ create-symlinks-tmux.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ln -s $HOME/code/utilities/tmux-split-window-dwm.sh $HOME/.local/bin/tmux-split-window-dwm
+ln -s $HOME/code/utilities/tmux-kill-pane-dwm.sh $HOME/.local/bin/tmux-kill-pane-dwm
