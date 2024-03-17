77e6c92b6968dec7e62d10e455364430a8880a22
TSomKes <tsomkes@gmail.com>
2024-03-17 16:56:58 -0500

    Change poke schedule to every 20 min

--- tmux-doing-poke.sh
+++ tmux-doing-poke.sh
@@ -3,6 +3,6 @@
 # Display a reminder to log a "doing" entry
 # So far, not being done via /etc/cron.d/.  Instead, just straight-up crontab.
 # $ crontab -e
-# */30 * * * * ~/code/utilities/tmux-poke.sh
+# */20 * * * * ~/code/utilities/tmux-poke.sh
 
 tmux display-message -d 30000 "#[fill=black bg=yellow align=centre]        * * *        Whatcha doin'?        * * *        " &
